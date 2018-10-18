b5e3207b57240b3fbfcb3b3d38dd3f8fc1926770
Penn Bauman <penn.bauman@gmail.com>
2018-10-18 19:55:10 -0400

    create curcuit checker program

--- curcuit-test/curcuit-test.ino
+++ curcuit-test/curcuit-test.ino
@@ -1,17 +1,35 @@
-int connectionPin = 9;
+const int connectionPin = A0;
+const int ledPin = 9;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(connectionPin, INPUT); 
+  pinMode(connectionPin, INPUT_PULLUP); 
+  pinMode(ledPin, OUTPUT); 
    
   Serial.println("READY");
+}
+
+void setLED(bool state) {
+  if (state) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
 }
 
 void loop() {
   int connectionState = analogRead(connectionPin);
   Serial.println(connectionState);
-  /*if (connectionState > 10) {
-    
-  } //*/
+  bool connected (connectionState > 200);
+  setLED(connected);
+  
   delay(500);
 }
+
+
+/* LAYOUT
+ * Transistor Emitter -> 330ohm resistor -> ground
+ *                    -> Checking wire (A0)
+ * Transistor Base -> 330ohm resistor -> end of checked curcuit
+ * Transistor Collector -> 5V+
+ */
